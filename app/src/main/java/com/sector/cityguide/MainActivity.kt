0c9e7a3a0e481c36d9858ebda637d97523e529a4
sector <75115941+onesector@users.noreply.github.com>
2022-01-18 01:20:29 +0300

    added bottom navigation and some major fragments

--- app/src/main/java/com/sector/cityguide/MainActivity.kt
+++ app/src/main/java/com/sector/cityguide/MainActivity.kt
@@ -2,6 +2,10 @@ package com.sector.cityguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sector.cityguide.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +15,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/sector/cityguide/fragments/ProfileFragment.kt
+++ app/src/main/java/com/sector/cityguide/fragments/ProfileFragment.kt
@@ -5,18 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.sector.cityguide.databinding.FragmentHomeBinding
+import com.sector.cityguide.R
+import com.sector.cityguide.databinding.FragmentProfileBinding
 
-class HomeFragment : Fragment() {
-    private var _binding: FragmentHomeBinding? = null
+class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
 
