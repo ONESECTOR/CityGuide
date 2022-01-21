37af010637f7097f84f41635e3cfada6ba0cd9a6
sector <75115941+onesector@users.noreply.github.com>
2022-01-21 23:07:57 +0300

    added function for email validation

--- app/src/main/java/com/sector/cityguide/fragments/login/LoginFragment.kt
+++ app/src/main/java/com/sector/cityguide/fragments/login/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.sector.cityguide.fragments.login
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,5 +23,45 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        emailFocusListener()
+        passwordFocusListener()
+    }
+
+    private fun passwordFocusListener() {
+        binding.etPassword.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.passwordContainer.helperText = validatePassword()
+            }
+        }
+    }
+
+    private fun validatePassword(): CharSequence? {
+        val password = binding.etPassword.text.toString()
+
+        // this function will update in the future
+
+        return null
+    }
+
+    private fun emailFocusListener() {
+        binding.etEmail.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.emailContainer.helperText = validateEmail()
+            }
+        }
+    }
+
+    private fun validateEmail(): CharSequence? {
+        val email = binding.etEmail.text.toString()
+
+        if (email.isEmpty()) {
+            return "Field can't be empty"
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return "Invalid Email Address"
+        }
+
+        return null
     }
 }
