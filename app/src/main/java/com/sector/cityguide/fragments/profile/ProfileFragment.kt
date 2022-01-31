f32b3b9d5c41fb65b050bc581bdb0e68a3687288
sector <75115941+onesector@users.noreply.github.com>
2022-02-01 00:44:47 +0300

    ProfileViewModel editing

--- app/src/main/java/com/sector/cityguide/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/sector/cityguide/fragments/profile/ProfileFragment.kt
@@ -5,20 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.sector.cityguide.R
 import com.sector.cityguide.databinding.FragmentProfileBinding
 import com.sector.cityguide.models.ProfileMenu
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var profileMenuList: MutableList<ProfileMenu>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,27 +29,12 @@ class ProfileFragment : Fragment() {
 
         val list = mutableListOf<ProfileMenu>()
 
-        //setupRecyclerView()
         val viewModel = ViewModelProvider(requireActivity()).get(ProfileViewModel::class.java)
         viewModel.getElements().observe(viewLifecycleOwner, Observer { element ->
             list.addAll(element)
         })
 
         val adapter = ProfileAdapter(list)
-        binding.rvProfileMenu.layoutManager = LinearLayoutManager(requireContext())
-        binding.rvProfileMenu.adapter = adapter
-    }
-
-    private fun setupRecyclerView() {
-        profileMenuList = mutableListOf(
-            ProfileMenu(
-                title = "Favorite",
-                description = "What you liked",
-                icon = R.drawable.ic_outline_favorite_light
-            )
-        )
-
-        val adapter = ProfileAdapter(profileMenuList)
         binding.rvProfileMenu.layoutManager = LinearLayoutManager(requireContext())
         binding.rvProfileMenu.adapter = adapter
     }
--- app/src/main/java/com/sector/cityguide/fragments/profile/ProfileViewModel.kt
+++ app/src/main/java/com/sector/cityguide/fragments/profile/ProfileViewModel.kt
@@ -7,35 +7,6 @@ import com.sector.cityguide.R
 import com.sector.cityguide.models.ProfileMenu
 
 class ProfileViewModel: ViewModel() {
-    /*private val mutableProfileMenu = MutableLiveData<MutableList<ProfileMenu>>()
-    val profileMenu: MutableLiveData<MutableList<ProfileMenu>> get() = mutableProfileMenu
-
-    init {
-        getProfileMenuList()
-    }
-
-    private fun getProfileMenuList() {
-        val list = mutableListOf(
-            ProfileMenu(
-                title = "Избранное",
-                description = "Просмотр того, что вам понравилось",
-                icon = R.drawable.ic_outline_favorite_light
-            ),
-            ProfileMenu(
-                title = "Профиль",
-                description = "Измените информацию о себе",
-                icon = R.drawable.ic_outline_profile_light
-            ),
-            ProfileMenu(
-                title = "Настройки",
-                description = "Общее в приложении",
-                icon = R.drawable.ic_outline_settings_light
-            )
-        )
-
-        mutableProfileMenu.value = list
-    }*/
-
     private var list = MutableLiveData<MutableList<ProfileMenu>>()
 
     init {
@@ -59,9 +30,16 @@ class ProfileViewModel: ViewModel() {
         )
         arrayList.add(
             ProfileMenu(
-                title = "Избранное",
-                description = "Просмотр того, что вам понравилось",
-                icon = R.drawable.ic_outline_favorite_light
+                title = "Профиль",
+                description = "Измените информацию о себе",
+                icon = R.drawable.ic_outline_profile_light
+            )
+        )
+        arrayList.add(
+            ProfileMenu(
+                title = "Настройки",
+                description = "Общее в приложении",
+                icon = R.drawable.ic_outline_settings_light
             )
         )
 
