6f65196f380fd64f37326bcdb77de733d7f456eb
sector <75115941+onesector@users.noreply.github.com>
2022-02-17 19:16:23 +0300

    added splash screen

---
+++ app/src/main/java/com/sector/cityguide/fragments/splash/SplashScreenFragment.kt
@@ -0,0 +1,32 @@
+package com.sector.cityguide.fragments.splash
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.sector.cityguide.R
+import com.sector.cityguide.databinding.FragmentSplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenFragment : Fragment() {
+    private var _binding: FragmentSplashScreenBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        findNavController().navigate(R.id.action_splashScreenFragment_to_homeFragment)
+    }
+}
