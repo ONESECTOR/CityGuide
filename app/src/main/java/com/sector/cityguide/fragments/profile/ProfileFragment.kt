ab62f672cb862943211700781513d289b1f7c100
sector <75115941+onesector@users.noreply.github.com>
2022-02-18 15:01:35 +0300

    re-designed profile page

--- app/src/main/java/com/sector/cityguide/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/com/sector/cityguide/fragments/profile/ProfileFragment.kt
@@ -5,19 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.sector.cityguide.databinding.FragmentProfileBinding
-import com.sector.cityguide.fragments.profile.adapter.ProfileAdapter
-import com.sector.cityguide.fragments.profile.viewmodel.ProfileViewModel
-import com.sector.cityguide.models.ProfileMenu
 
 class ProfileFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
-
-    private lateinit var adapter: ProfileAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,30 +17,11 @@ class ProfileFragment : Fragment() {
     ): View {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
 
-        setupRecyclerView()
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        fillMenu()
-    }
-
-    private fun fillMenu() {
-        val list = mutableListOf<ProfileMenu>()
-
-        val viewModel = ViewModelProvider(requireActivity()).get(ProfileViewModel::class.java)
-        viewModel.getMenuElements().observe(viewLifecycleOwner, Observer { menu ->
-            list.addAll(menu)
-            adapter.submitList(list)
-        })
-    }
-
-    private fun setupRecyclerView() {
-        adapter = ProfileAdapter()
-        binding.rvProfileMenu.layoutManager = LinearLayoutManager(requireContext())
-        binding.rvProfileMenu.adapter = adapter
     }
 }
--- app/src/main/java/com/sector/cityguide/models/ProfileMenu.kt
+++
@@ -1,7 +0,0 @@
-package com.sector.cityguide.models
-
-data class ProfileMenu(
-    val title: String = "",
-    val description: String = "",
-    val icon: Int
-)
