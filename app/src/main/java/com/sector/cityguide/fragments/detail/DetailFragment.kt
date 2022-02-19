8e9a0e53e5eadce4f4315573ff44a1761d56a163
sector <75115941+onesector@users.noreply.github.com>
2022-02-19 12:13:05 +0300

    added detail page

---
+++ app/src/main/java/com/sector/cityguide/fragments/detail/DetailFragment.kt
@@ -0,0 +1,26 @@
+package com.sector.cityguide.fragments.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sector.cityguide.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
