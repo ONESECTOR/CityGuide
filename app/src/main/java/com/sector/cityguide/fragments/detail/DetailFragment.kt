952273ecff057a7d22c67ff0a69f50269e7eed75
sector <75115941+onesector@users.noreply.github.com>
2022-02-22 14:46:20 +0300

    added a system that allows you to add to favorite places

--- app/src/main/java/com/sector/cityguide/fragments/detail/DetailFragment.kt
+++ app/src/main/java/com/sector/cityguide/fragments/detail/DetailFragment.kt
@@ -8,10 +8,12 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.sector.cityguide.R
 import com.sector.cityguide.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
@@ -19,6 +21,9 @@ class DetailFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args by navArgs<DetailFragmentArgs>()
+
+    private lateinit var auth: FirebaseAuth
+    private var isInFavorite = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,12 +37,99 @@ class DetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initFirebase()
         loadPlaceDetails()
+
+        if (auth.currentUser != null) {
+            checkIsInFavorite()
+        }
+
+        binding.btnBack.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+        binding.btnAddToFavorite.setOnClickListener {
+            if (auth.currentUser == null) {
+                Toast.makeText(requireContext(), "Вы не вошли в аккаунт!", Toast.LENGTH_SHORT).show()
+            } else {
+                if (isInFavorite) {
+                    removeFromFavorite()
+                } else {
+                    addToFavorite()
+                }
+            }
+        }
+    }
+
+    private fun addToFavorite() {
+        val hashMap = HashMap<String, Any>()
+
+        with(args) {
+            hashMap["id"] = getPlaceId()
+            hashMap["name"] = currentPlace.name
+            hashMap["location"] = currentPlace.description
+            hashMap["image"] = currentPlace.image
+            hashMap["description"] = currentPlace.description
+        }
+
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(getProfileUid()!!)
+            .child("Favorites")
+            .child(getPlaceId())
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(), "Успешно добавлено в избранное!", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(requireContext(), "Ошибка!", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkIsInFavorite() {
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(getProfileUid()!!)
+            .child("Favorites")
+            .child(getPlaceId())
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    isInFavorite = snapshot.exists()
+
+                    if (isInFavorite) {
+                        binding.btnAddToFavorite.setImageResource(R.drawable.ic_filled_favorite_red)
+                    } else {
+                        binding.btnAddToFavorite.setImageResource(R.drawable.ic_outline_favorite)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+    }
+
+    private fun removeFromFavorite() {
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(getProfileUid()!!)
+            .child("Favorites")
+            .child(getPlaceId())
+            .removeValue()
+            .addOnSuccessListener {
+                Toast.makeText(requireContext(), "Успешно удалено!", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                Toast.makeText(requireContext(), "Ошибка при удалении!", Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun getPlaceId(): String {
         return args.currentPlace.id.toString()
     }
+
+    private fun initFirebase() {
+        auth = FirebaseAuth.getInstance()
+    }
+
+    private fun getProfileUid() = auth.uid
 
     private fun loadPlaceDetails() {
         val ref = FirebaseDatabase.getInstance().getReference("Places")
