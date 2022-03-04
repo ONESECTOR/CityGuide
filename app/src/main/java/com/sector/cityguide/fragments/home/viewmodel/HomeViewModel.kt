a28b439f6ba2d5864a9e0d3e452a3c9db0fb2edd
sector <75115941+onesector@users.noreply.github.com>
2022-03-04 20:40:05 +0300

    fixed bug with greeting on home screen

--- app/src/main/java/com/sector/cityguide/fragments/home/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/sector/cityguide/fragments/home/viewmodel/HomeViewModel.kt
@@ -1,5 +1,6 @@
 package com.sector.cityguide.fragments.home.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -12,15 +13,21 @@ class HomeViewModel: ViewModel() {
         return _greeting
     }
 
-    fun getGreetingMessage() {
+    init {
+        getGreetingMessage()
+    }
+
+    private fun getGreetingMessage() {
         val c = Calendar.getInstance()
         val timeOfDay = c.get(Calendar.HOUR_OF_DAY)
+        Log.d("Time", timeOfDay.toString())
 
         _greeting.value = when(timeOfDay) {
             in 6..11 -> R.string.good_morning
             in 12..17 -> R.string.good_afternoon
             in 18..21 -> R.string.good_evening
-            in 22 downTo 5 -> R.string.good_night
+            in 22..23 -> R.string.good_night
+            in 0..5 -> R.string.good_night
             else -> R.string.hello
         }
     }
